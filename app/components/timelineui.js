fd1f217d4962ed473bd8c772e37a21b510ea7112
s-afrid <afrid0052@gmail.com>
2025-12-24 17:59:46 +0530

    added timeline

---
+++ app/components/timelineui.js
@@ -0,0 +1,27 @@
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+export default function AlternateTimeline() {
+  return (
+    <Timeline className='text-white' position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Exicom Technologies</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>U R Rao Satellite Centre, ISRO</TimelineContent>
+      </TimelineItem>
+      
+    </Timeline>
+  );
+}
--- app/timeline/page.js
+++ app/timeline/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AlternateTimeline from "../components/timelineui";
 const page = () => {
   
 
@@ -9,6 +10,7 @@ const page = () => {
         Timeline
       </h1>
 
+    <AlternateTimeline />
      
     </div>
   );
