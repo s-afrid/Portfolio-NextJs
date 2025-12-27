baa6fef03b51c3d386ab3e4c9e6d744e4d134d95
s-afrid <afrid0052@gmail.com>
2025-12-27 09:16:43 +0530

    Implement message submission with Telegram API and add toast notifications for user feedback

--- app/actions/message.js
+++ app/actions/message.js
@@ -1,7 +1,41 @@
 "use server"
 
-export const submitActions = async (e)=>{
+export const submitActions = async (formData)=>{
   
-    console.log(e.get("name"), e.get("email"),e.get("message"))
- 
-}
+const message = formData.get('message'); 
+const email = formData.get('email');
+const name = formData.get('name')
+
+if (!message || !email) {
+    return { success: false, error: 'Missing fields' };
+  }
+  
+  const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  const chatId = process.env.TELEGRAM_CHAT_ID;
+  
+  const text = `New Message:\nEmail: ${email}\nName: ${name}\nMessage: ${message}`;
+
+  try {
+    const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: text,
+      }),
+    });
+
+    if (!response.ok) {
+        throw new Error('Telegram API Error');
+    }
+
+    return { success: true };
+
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: 'Failed to send message' };
+  }
+
+  };
--- app/components/message.js
+++ app/components/message.js
@@ -1,15 +1,65 @@
 "use client"
 import { submitActions } from "../actions/message";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+ import { ToastContainer, toast } from 'react-toastify';
 import React from 'react'
 
 const Message = () => {
+
+  const [status, setStatus] = useState(null);
+
+  async function handleSubmit(formData) {
+    setStatus('sending');
+    
+    
+    const result = await submitActions(formData);
+
+    if (result.success) {
+      toast.success('Sent to telegram', {
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+});
+      setStatus('success');
+    } else {
+      toast.error('Failed to send', {
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+});
+      setStatus('error');
+    }
+  }
+
      const ref = useRef()
   return (
     <div className="bg-gray-800 p-4 rounded-2xl w-[95%] md:w-[60%] 2xl:w-[50%] border border-white [box-shadow:3px_3px_#ffffff]">
 
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+
         <h3 className="font-jetbrains text-xl tracking-wider mb-3">Please leave a message</h3>
-        <form className="flex flex-col gap-4 w-full" ref={ref} action={(e)=>{submitActions(e); ref.current.reset()}}>
+        <form className="flex flex-col gap-4 w-full" ref={ref} action={(e)=>{handleSubmit(e); ref.current.reset()}}>
 
         <div className="flex gap-2 justify-between font-tech">
         <label htmlFor="name">Name</label>
@@ -26,7 +76,7 @@ const Message = () => {
         <textarea className="text-white bg-gray-700 w-[230px] md:w-[85%] h-[100px] rounded-xl p-3 ring-1 ring-gray-500" name="message" id="message" placeholder="Your message"/>
         </div>
         <div>
-          <button className="bg-white text-black p-2 w-[100px] rounded-3xl font-tech [box-shadow:3px_3px_#8e9091] hover:scale-105 transition-transform duration-300 cursor-pointer">Submit</button>
+          <button disabled={status === 'sending'}  className="bg-white text-black p-2 w-[100px] rounded-3xl font-tech [box-shadow:3px_3px_#8e9091] hover:scale-105 transition-transform duration-300 cursor-pointer">{status === 'sending' ? 'Submitting...' : 'Submit'}</button>
         </div>
       </form>
     </div>
